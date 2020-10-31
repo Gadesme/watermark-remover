fa25572543adeaee5210c57b8411486832f3fe9b
kharaone <kharaone@gmail.com>
2020-10-31 23:32:05 +0100

    fic(#5): update usage of tg

--- model.py
+++ model.py
@@ -34,8 +34,8 @@ class UpdateAnnealingParameter(Callback):
 
 
 def tf_log10(x):
-    numerator = tf.log(x)
-    denominator = tf.log(tf.constant(10, dtype=numerator.dtype))
+    numerator = tf.math.log(x)
+    denominator = tf.math.log(tf.constant(10, dtype=numerator.dtype))
     return numerator / denominator
 
 
